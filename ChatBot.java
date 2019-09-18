dfc29d2b37a51c41d9d0fcf609c06c18cc4a9b2f
hypelive <48682209+hypelive@users.noreply.github.com>
2019-09-18 23:07:31 +0500

    probably it's final

--- ChatBot.java
+++ ChatBot.java
@@ -4,7 +4,8 @@ import java.util.function.Function;
 public class ChatBot
 {
     private String name;
-    public HashMap<String, Function<String, String>> Comands = new HashMap<>();
+    private boolean alive;
+    public HashMap<String, Function<String, String>> Commands = new HashMap<>();
 
     public String echo(String txt)
     {
@@ -19,18 +20,53 @@ public class ChatBot
     public String help(String txt)
     {
         String result = "";
-        for (var key : Comands.keySet())
+        for (var key : Commands.keySet())
         {
             result += key + " ";
         }
         return result;
     }
 
+    public String bye(String txt)
+    {
+        this.alive = false;
+        return "bye";
+    }
+
+    public String rap(String txt)
+    {
+        //generate_text();
+        return "To be, or not to be: that is the question:\n" +
+                "Whether 'tis nobler in the mind to suffer\n" +
+                "The slings and arrows of outrageous fortune,\n" +
+                "Or to take arms against a sea of troubles,\n" +
+                "And by opposing end them? To die: to sleep;\n" +
+                "No more; and by a sleep to say we end\n" +
+                "The heart-ache and the thousand natural shocks\n" +
+                "That flesh is heir to, 'tis a consummation\n" +
+                "Devoutly to be wish'd. To die, to sleep;\n" +
+                "To sleep: perchance to dream: ay, there's the rub;\n" +
+                "For in that sleep of death what dreams may come\n" +
+                "When we have shuffled off this mortal coil,\n" +
+                "Must give us pause: there's the respect\n" +
+                "That makes calamity of so long life;";
+    }
+
+    public boolean is_alive()
+    {
+        return this.alive;
+    }
+
     ChatBot(String name)
     {
         this.name = name;
-        Comands.put("echo", this::echo);
-        Comands.put("getName", this::getName);
-        Comands.put("help", this::help);
+        this.alive = true;
+        System.out.print("Yo I'm " + name + "\nAt your service...\n");
+        Commands.put("echo", this::echo);
+        Commands.put("getName", this::getName);
+        Commands.put("help", this::help);
+        Commands.put("bye", this::bye);
+        Commands.put("rap", this::rap);
+
     }
 }
--- Program.java
+++ Program.java
@@ -6,19 +6,20 @@ public class Program
     public static void main(String[] args)
     {
         var bot = new ChatBot("Pavel");
-        while (true)
+        while (bot.is_alive())
         {
             Scanner scan = new Scanner(System.in);
             String name = scan.next();
             String arg = "";
             if (scan.nextLine().split(" ").length >= 2)
-                arg = scan.nextLine().split(" ")[1];
+                arg = scan.nextLine().split(" ")[1];      //видимо, тут не то, что задумывалось
             String result;
-            if (bot.Comands.containsKey(name))
-                result = bot.Comands.get(name).apply(arg);
+            if (bot.Commands.containsKey(name))
+                result = bot.Commands.get(name).apply(arg);
             else
                 result = "This command is undefined";
             System.out.print(result + "\n");
+            //Thread.sleep(30); norm tema but douesn't works
         }
     }
 }
